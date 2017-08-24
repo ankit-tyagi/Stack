3416fdd744920d482bf30c6312a48589421d6dd6
Ankit Tyagi <ankittyaagi@gmail.com>
2017-08-24 17:35:27 +0530

    tile started moving

--- Assets/Scripts/TheStack.cs
+++ Assets/Scripts/TheStack.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class TheStack : MonoBehaviour {
 
+	private const float BOUNCE_SIZE = 3.5f;
+	private const float STACK_MOVING_SPEED = 5.0f;
+
 	private GameObject[] theStack;
 
 	private int stackIndex ;
 	private int scorecount = 0;
+
+	private float tileTransition = 0.0f;
+	private float tileSpeed = 2.5f;
+	private float secondaryposition;
+
+	private bool isMovingOnX = true;
+
+	private Vector3 desiredPosition;
+
 
 	// Use this for initialization
 	private void Start () {
@@ -21,23 +33,54 @@ public class TheStack : MonoBehaviour {
 	// Update is called once per frame
 	private void Update () {
 		if (Input.GetMouseButtonDown (0)) {
-			SpawnTile ();
-			scorecount++;
+			if (PlaceTile ()) {
+				SpawnTile ();
+				scorecount++;
+			} else {
+				EndGame ();
+			}
 		}
+
+		MoveTile ();
+
+		transform.position = Vector3.Lerp (transform.position, desiredPosition, STACK_MOVING_SPEED * Time.deltaTime);
+	}
+
+	private void MoveTile() {
+		tileTransition += Time.deltaTime * tileSpeed; 
+
+		if (isMovingOnX) {
+			theStack [stackIndex].transform.localPosition = new Vector3 (Mathf.Sin (tileTransition) * 1.25f, scorecount, secondaryposition);
+		} else {
+			theStack [stackIndex].transform.localPosition = new Vector3 (secondaryposition, scorecount, Mathf.Sin (tileTransition) * 1.25f);
+		}
+
 	}
 
 	private void SpawnTile() {
 		stackIndex--;
 		if (stackIndex < 0)
 			stackIndex = transform.childCount - 1;
-		
+
+		desiredPosition = (Vector3.down) * scorecount;
+
 		theStack [stackIndex].transform.localPosition = new Vector3 (0, scorecount, 0);
 
 	}
 
-	private void PlaceTile(){
-		theStack [stackIndex].transform.localPosition = new Vector3 (0, scorecount, 0);
+	private bool PlaceTile(){
+		
+		Transform t = theStack [stackIndex].transform;
 
+		secondaryposition = (isMovingOnX)
+			? t.localPosition.x
+			: t.localPosition.z;
+		
+		isMovingOnX = !isMovingOnX;
+		return true;
+	}
+
+	private void EndGame () {
 	}
 		
 }
