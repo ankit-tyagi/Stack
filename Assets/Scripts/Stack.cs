6e27a7374e0f3ae26f8b7c1d516f70196eeaf1df
Ankit Tyagi <ankittyaagi@gmail.com>
2017-08-26 17:28:30 +0530

    changes added only one problem of rebel remain

--- Assets/Scripts/Stack.cs
+++ Assets/Scripts/Stack.cs
@@ -3,14 +3,14 @@ using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class TheStack : MonoBehaviour {
+public class Stack : MonoBehaviour {
 
 	public Text scoreText;
 	public Color32[] gameColors = new Color32[4];
 	public Material stackMaterial;
 	public GameObject endPanel;
 
-	private const float BOUND_SIZE = 3.0f;
+	private const float BOUND_SIZE = 3f;
 	private const float STACK_MOVING_SPEED = 5.0f;
 	private const float ERROR_MARGIN = 0.1f;
 
@@ -22,14 +22,14 @@ public class TheStack : MonoBehaviour {
 	private int combo = 0;
 
 	private float tileTransition = 0.0f;
-	private float tileSpeed = 2.5f;
+	private float tileSpeed = 1.5f;
 	private float secondaryPosition;
 
 	private Vector3 desiredPosition;
 	private Vector3 lastTilePosition;
 
 	private bool isMoveOnX = true;
-	private bool gameOver = false;
+	private bool isDead = false;
 
 	private void Start () {
 		theStack = new GameObject[transform.childCount];
@@ -43,7 +43,7 @@ public class TheStack : MonoBehaviour {
 
 	private void Update () {
 
-		if (gameOver)
+		if (isDead)
 			return;
 
 		if (Input.GetMouseButtonDown (0)) {
@@ -54,7 +54,7 @@ public class TheStack : MonoBehaviour {
 			} else {
 				EndGame ();
 			}
-
+		
 		}
 
 		MoveTile ();
@@ -178,7 +178,7 @@ public class TheStack : MonoBehaviour {
 		if (PlayerPrefs.GetInt ("score") < scoreCount) {
 			PlayerPrefs.SetInt ("score", scoreCount);
 		}
-		gameOver = true;
+		isDead = true;
 		endPanel.SetActive (true);
 		theStack [stackIndex].AddComponent<Rigidbody> ();
 	}
@@ -208,145 +208,3 @@ public class TheStack : MonoBehaviour {
 		mesh.colors32 = colors;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//
-//public class TheStack : MonoBehaviour {
-//
-//	private const float BOUNCE_SIZE = 3f;
-//	private const float STACK_MOVING_SPEED = 5.0f;
-//	private const float ERROR_MARGIN = 0.1f;
-//
-//	private GameObject[] theStack;
-//	private Vector2 stackBounds = new Vector2 (BOUNCE_SIZE, BOUNCE_SIZE);
-//
-//	private int stackIndex ;
-//	private int scoreCount = 0;
-//	private int combo = 0;
-//
-//	private float tileTransition = 0.0f;
-//	private float tileSpeed = 2.5f;
-//	private float secondaryposition;
-//
-//	private bool isMovingOnX = true;
-//	private bool gameOver = false;
-//
-//	private Vector3 desiredPosition;
-//	private Vector3 lastTilePosition;
-//
-//	// Use this for initialization
-//	private void Start () {
-//		theStack = new GameObject[transform.childCount];
-//		for (int i = 0; i < transform.childCount; i++)
-//			theStack [i] = transform.GetChild (i).gameObject;
-//
-//		stackIndex = transform.childCount - 1;
-//	}
-//	
-//	// Update is called once per frame
-//	private void Update () {
-//		if (Input.GetMouseButtonDown (0)) {
-//			if (PlaceTile ()) {
-//				SpawnTile ();
-//				scoreCount++;
-//			} else {
-//				EndGame ();
-//			}
-//		}
-//
-//		MoveTile ();
-//
-//		transform.position = Vector3.Lerp (transform.position,desiredPosition,STACK_MOVING_SPEED * Time.deltaTime);
-//	}
-//
-//	private void MoveTile() {
-//		if (gameOver)
-//			return;  
-//
-//		tileTransition += Time.deltaTime * tileSpeed; 
-//
-//		if (isMovingOnX) {
-//			theStack [stackIndex].transform.localPosition = new Vector3 (Mathf.Sin(tileTransition) *BOUNCE_SIZE, scoreCount, secondaryposition);
-//		} else {
-//			theStack [stackIndex].transform.localPosition = new Vector3 (secondaryposition, scoreCount, Mathf.Sin(tileTransition) *BOUNCE_SIZE);
-//		}
-//
-//	}
-//
-//	private void SpawnTile() {
-//		lastTilePosition = theStack [stackIndex].transform.localPosition; 
-//		stackIndex--;
-//		if (stackIndex < 0)
-//			stackIndex = transform.childCount - 1;
-//
-//		desiredPosition = (Vector3.down) * scoreCount;
-//
-//		theStack [stackIndex].transform.localPosition = new Vector3 (0, scoreCount, 0);
-//
-//	}
-//
-//	private bool PlaceTile(){
-//		
-//		Transform t = theStack [stackIndex].transform;
-//
-//		if (isMovingOnX) {
-//			float deltaX = lastTilePosition.x - t.position.x;
-//			if (Mathf.Abs (deltaX) > ERROR_MARGIN) {
-//				//cut the tile
-//				combo = 0;
-//				stackBounds.x -= Mathf.Abs (deltaX);
-//				if (stackBounds.x <= 0) 
-//					return false;
-//
-//				float middle = lastTilePosition.x + t.localPosition.x / 2;
-//				t.localScale = new Vector3 (stackBounds.x, 1, stackBounds.y);
-//				t.localPosition = new Vector3 (middle - (lastTilePosition.x /2), scoreCount, lastTilePosition.z);
-//			}
-//		} else {
-//			float deltaZ = lastTilePosition.z - t.position.z;
-//			if (Mathf.Abs (deltaZ) > ERROR_MARGIN) {
-//
-//				//cut the tile
-//				combo = 0;
-//				stackBounds.y -= Mathf.Abs (deltaZ);
-//				if (stackBounds.y <= 0)
-//					return false;
-//
-//				float middle = lastTilePosition.z + t.localPosition.z / 2;
-//				t.localScale = new Vector3 (stackBounds.x, 1, stackBounds.y);
-//				t.localPosition = new Vector3 ( lastTilePosition.x, scoreCount, middle - (lastTilePosition.z /2 ));
-//			}
-//		}
-//
-//		secondaryposition = (isMovingOnX)
-//			? t.localPosition.x
-//			: t.localPosition.z;
-//		
-//		isMovingOnX = !isMovingOnX;
-//		return true;
-//	}
-//
-//	private void EndGame () {
-//		Debug.Log ("Lose");
-//		gameOver = true;
-//		theStack [stackIndex].AddComponent<Rigidbody> ();
-//	}
-//		
-//}
-//
-//
